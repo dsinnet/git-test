3097d6c5f363dabe8cea30908231ea4ff96105ff
dsinnet <rifle-ds@Rifle-DS.local>
2015-02-09 16:41:29 +0000

    New file added

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+
+<!-- CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/styles.min.css" rel="stylesheet">
+<link href="css/MyFontsWebfontsKit.css" rel="stylesheet">
+<link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- Typekit Fonts -->
+<script src="//use.typekit.net/qjw3gms.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
+</head>
+
+<body>
+
+<?php include("includes/inc-header.php") ?>
+
+<section class="dotted">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Digital Design & Development<br>Marketing Support<br>Brand Communications</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</h2>
+			</div>
+			<div class="col-sm-6">
+				<a href="#" class="btn btn-light">expertise <i class="glyphicon glyphicon-play"></i></a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="recent-projects margin-bottom-0">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h3 class="text-center">Recent Projects</h3>
+			</div>
+			
+			<?php include("includes/inc-recent-projects.php") ?>
+			
+		</div>
+	</div>
+</section>
+
+<?php include("includes/inc-footer.php") ?>
+
+<!-- JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+<script src="js/retina.min.js"></script>
+<script src="js/scripts.min.js"></script>
+</body>
+</html>
